2611cbd1d751eb57c24477b5bcb2d5fdfe545ee8
LovinKuruvilla <lovinkuruvilla@yahoo.com>
2018-04-10 10:10:29 -0500

    Created request page.
    s

---
+++ WebApplication42/WebApplication42/Models/CreateEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication42.Models
+{
+    public class CreateEvent
+    {
+        //Event Table
+        string Description;
+        int UserID;
+        int EventID;
+
+        //Event Address Table
+        string street;
+        string street2;
+        string city;
+        string state;
+        string zipcode;
+    }
+}

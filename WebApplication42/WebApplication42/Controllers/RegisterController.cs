1635931dc3fc8e5520d6b630435c21d8b8ecd39c
LovinKuruvilla <lovinkuruvilla@yahoo.com>
2018-04-09 19:46:56 -0500

    Clean slate files. Please delete the .cs files in model and views according to your needs

---
+++ WebApplication42/WebApplication42/Controllers/RegisterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication42.Models;
+
+namespace WebApplication42.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Volunteer
+        public ActionResult Index()
+        {
+            return RedirectToAction("Register");
+        }
+
+        // GET: Volunteer/Create
+        public ActionResult Register()
+        {
+            return View(new voltab());
+        }
+
+        // POST: Volunteer/Create
+        [HttpPost]
+        public ActionResult Register(voltab v)
+        {
+            //if(ModelState.IsValid)
+            //{ 
+
+            using (DBModels s = new DBModels())
+            {
+                
+            }
+            return RedirectToAction("AccountRegistered");
+
+
+            //If there were errors in data return view
+        //}return View(v);
+        }
+
+        //successfull registration
+        public ActionResult AccountRegistered()
+        {
+            return View();
+
+        }
+
+        
+        // GET: Volunteer/Edit/5
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        // POST: Volunteer/Edit/5
+        //[HttpPost]
+        //public ActionResult login()
+        //{
+            
+        //    ViewBag.Message = "Wrong username or password";
+        //    return View();
+        //}
+
+        
+
+       
+
+        
+    }
+}
---
+++ WebApplication42/WebApplication42/Controllers/RequestController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication42.Models;
+
+namespace WebApplication42.Controllers
+{
+    
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Request/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Request/Create
+        public ActionResult Create()
+        {
+            return View(new reqtab());
+        }
+
+        // POST: Request/Create
+        [HttpPost]
+        public ActionResult Create(reqtab v)
+        {
+            
+
+            //We have an error return the model to the view
+            return View();//v);
+        }
+        public ActionResult Posted()
+        {
+            return View();
+        }
+
+        // GET: Request/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Request/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Request/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Request/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
